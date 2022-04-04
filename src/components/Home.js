5b6ae763ed8cae3d8ded61b5774bdb50f8695189
vuoso-l <vuosolucas@gmail.com>
2022-04-04 18:35:31 -0300

    feat: added one paragraph

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { H2Style, HomepageSection, ParagraphHome } from "../componentsStyle/BasicTagsStyle";
+import {
+  H2Style,
+  HomepageSection,
+  ParagraphHome,
+} from "../componentsStyle/BasicTagsStyle";
 
 const Home = () => {
   return (
@@ -11,6 +15,10 @@ const Home = () => {
           en el buscador para conformar la carta completa del restaurant. Tené
           en cuenta que podrás elegir hasta cuatro platos, de los cuales, dos
           deben ser veganos.
+        </p>
+        <p>
+          Tené en cuenta que las palabras a utilizar en el buscador deben ser en
+          inglés para que el mismo las tome.
         </p>
         <p>
           Si querés obtener más información sobre recetas y comidas, hace{" "}
